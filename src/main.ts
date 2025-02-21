71163fa1c19af4cee62397af77bdd6c840edf510
Nelsonzbs <zhangbaisong@88.com>
2025-02-21 18:19:57 +0800

    基础配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+/*
+ * @Author: Nelsonzbs zhangbaisong@88.com
+ * @Date: 2025-02-21 16:32:28
+ * @LastEditors: Nelsonzbs zhangbaisong@88.com
+ * @LastEditTime: 2025-02-21 18:08:41
+ * @FilePath: \Nelson-Vue-Blog\src\main.ts
+ * @Description: 项目入口
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount("#app");
