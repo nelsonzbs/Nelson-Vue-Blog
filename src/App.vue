956eba92c3991e2522929dbb1741e126c9109077
Nelsonzbs <zhangbaisong@88.com>
2025-02-25 17:24:38 +0800

    配置element-plus按需引入

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: Nelsonzbs zhangbaisong@88.com
  * @Date: 2025-02-21 16:32:28
  * @LastEditors: Nelsonzbs zhangbaisong@88.com
- * @LastEditTime: 2025-02-24 14:50:47
+ * @LastEditTime: 2025-02-25 17:22:13
  * @FilePath: \Nelson-Vue-Blog\src\App.vue
  * @Description: 项目根组件
 -->
@@ -12,6 +12,14 @@
     <router-link to="/main">主要页面</router-link>
     <router-link to="/login">登录页面</router-link>
     <router-view></router-view>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,31 @@
  * @Author: Nelsonzbs zhangbaisong@88.com
  * @Date: 2025-02-21 16:32:28
  * @LastEditors: Nelsonzbs zhangbaisong@88.com
- * @LastEditTime: 2025-02-24 09:39:05
+ * @LastEditTime: 2025-02-25 17:19:35
  * @FilePath: \Nelson-Vue-Blog\vite.config.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 项目配置
  */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // 安装SRC别名依赖
 import path from "path";
+// 配置Element-plus自动按需导入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 以下AutoImport/Components是为配置Element-plus自动按需导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
